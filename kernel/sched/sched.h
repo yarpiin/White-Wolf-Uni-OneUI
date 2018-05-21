b34c3abfc72af3bc94cd38b06ea07f5c4aa58dd2
Jesse Chan <jc@lineageos.org>
2018-05-20 22:05:08 -0700

    sched: correct definition of dirty_sched_domain_sysctl
    Change-Id: If19ac569b329aebc90330d41aaf3a7125e48f8cb

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1078,7 +1078,7 @@ void unregister_sched_domain_sysctl(void);
 static inline void register_sched_domain_sysctl(void)
 {
 }
-static inline void dirty_sched_domain_sysctl(void)
+static inline void dirty_sched_domain_sysctl(int cpu)
 {
 }
 static inline void unregister_sched_domain_sysctl(void)
