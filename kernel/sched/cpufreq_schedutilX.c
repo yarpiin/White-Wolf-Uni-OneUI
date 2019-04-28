51e5730913272f64575851b2ed85d01e2d78cff5
Slawek <yarpiin@gmail.com>
2019-04-28 12:56:32 +0100

    schedutilX: hardcode up and down rates

--- kernel/sched/cpufreq_schedutilX.c
+++ kernel/sched/cpufreq_schedutilX.c
@@ -697,8 +697,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 	} else {
 		unsigned int lat;
 
-                tunables->up_rate_limit_us = LATENCY_MULTIPLIER;
-                tunables->down_rate_limit_us = LATENCY_MULTIPLIER;
+                tunables->up_rate_limit_us = 200;
+                tunables->down_rate_limit_us = 100;
 		lat = policy->cpuinfo.transition_latency / NSEC_PER_USEC;
 		if (lat) {
                         tunables->up_rate_limit_us *= lat;
